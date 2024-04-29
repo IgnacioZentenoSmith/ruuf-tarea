9bfdd2de9f714b7ee0a6571a306b2a93ac2c92cd
Rodrigo Ignacio Zenteno Smith <rzenteno@mmlv.cl>
2024-04-29 10:50:55 -0400

    cors & validation pipe to use DTOs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('backend'); // This adds /backend to all routes
+  app.enableCors(); // Enable CORS for all routes
+  app.useGlobalPipes(
+    new ValidationPipe({ // Automatically transform payloads to be instances of DTO classes
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
